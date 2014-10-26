8e82e1983cb50711ec3d825c4e963de810022049
Sandro Wong <swongkongfun@csupomona.edu>
2014-10-26 14:41:41 -0700

    Default Bootstrap with less

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    less: {
+      development: {
+        options: {
+          paths: ["build/css"]
+        },
+        files: {
+          "build/css/style.css": "components/less/bootstrap.less"
+        }
+      },
+      production: {
+        options: {
+          paths: ["build/css"],
+          cleancss: true
+        },
+        files: {
+          "build/css/style.min.css": ["components/less/bootstrap.less","components/css/jumbotron.css"]
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task(s).
+  grunt.registerTask('default', ['less']);
+
+};
