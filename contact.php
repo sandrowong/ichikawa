57105a1acfbd48444428bf47a6e05a2baf833294
Sandro Wong <swongkongfun@csupomona.edu>
2014-11-07 11:03:49 -0800

    Revert "Created php framework, fixed carrousel, added missing js for Win8 bug and cleaned up html."
    This reverts commit 516c84d5c052dfe4e76ac818b15b1bd8775082a1.
    
    Conflicts:
    	header.php
    	home.php
    
    reverted commit

--- contact.php
+++
@@ -1,5 +0,0 @@
-    <div class="container marketing">   
-      <div class="starter-template" align="center">
-        <br><br><br><br><h1>Contact Us</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
--- menu.php
+++
@@ -1,5 +0,0 @@
-    <div class="container marketing">      
-	  <div class="starter-template" align="center">
-        <br><br><br><br><h1>The Menu</h1>
-        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
-      </div>
