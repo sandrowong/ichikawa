e9942bf34e6260d56b5b1d47e0d166d8a752e479
Sandro Wong <swongkongfun@csupomona.edu>
2014-11-09 22:15:18 -0800

    separate PHP from home.php
    Created feature.php and marketing.php. Will be easier for the
    maintainance.

---
+++ feature.php
@@ -0,0 +1,42 @@
+    <!-- FEATURETTES   
+    ================================================== -->
+
+    <section class="container marketing"> <!-- Container -->
+
+      <hr class="featurette-divider">
+
+      <section class="row featurette bg-feature1"> <!-- feature1.jpg -->
+        <div class="col-md-7">
+          <h2 class="featurette-heading">First featurette heading.</h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+      </section> <!-- End of feature1 -->
+
+      <hr class="featurette-divider">
+
+      <section class="row featurette bg-feature2"> <!-- feature2.jpg -->
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+        <div class="col-md-7">
+          <h2 class="featurette-heading">Oh yeah</h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+      </section> <!-- End of feature2 -->
+
+      <hr class="featurette-divider">
+
+      <section class="row featurette bg-feature3"> <!-- feature3.jpg -->
+        <div class="col-md-7">
+          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
+          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
+        </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+        </div>
+      </section> <!-- End of feature3 -->
+
+      <hr class="featurette-divider">
+    
+    </section><!-- End of Container -->
+    <!-- END OF FEATURETTES -->
---
+++ marketing.php
@@ -0,0 +1,31 @@
+    <!-- MARKETING
+    ================================================== -->
+
+    <section class="container marketing"> <!-- Container -->
+
+      <!-- Three columns of text below the carousel -->
+      <div class="row"> <!-- MARKETING -->
+
+        <section class="col-lg-4 mark-box"> <!-- marketing1.jpg -->
+          <img class="img-circle" src="img/marketing1.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </section><!-- End of marketing1 -->
+
+        <section class="col-lg-4 mark-box"> <!-- marketing2.jpg -->
+          <img class="img-circle" src="img/marketing2.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+          <h2>Heading</h2>
+          <p>Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </section><!-- End of marketing2 -->
+
+        <section class="col-lg-4 mark-box"> <!-- marketing3.jpg -->
+          <img class="img-circle" src="img/marketing3.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
+          <h2>Heading</h2>
+          <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+        </div><!-- End of marketing3 -->
+
+      </div><!-- END OF MARKETING -->
+    </section> <!-- End of Container --> 
