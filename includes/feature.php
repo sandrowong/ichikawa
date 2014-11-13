eed6e83b10dbb335179c85a20eab6dd9f4792c71
Sandro Wong <swongkongfun@csupomona.edu>
2014-11-13 15:56:45 -0800

    fixed featured

--- includes/feature.php
+++ includes/feature.php
@@ -10,13 +10,16 @@
           <h2 class="featurette-heading">First featurette heading.</h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
+        <div class="col-md-5">
+          <img class="featurette-image img-responsive" src="img/feature1.jpg" alt="Happy Hour">
+        </div>        
       </section> <!-- End of feature1 -->
 
       <hr class="featurette-divider">
 
       <section class="row featurette bg-feature2"> <!-- feature2.jpg -->
         <div class="col-md-5">
-          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
+          <img class="featurette-image img-responsive" src="img/feature2.jpg" alt="Dine for two">
         </div>
         <div class="col-md-7">
           <h2 class="featurette-heading">Oh yeah</h2>
@@ -25,18 +28,6 @@
       </section> <!-- End of feature2 -->
 
       <hr class="featurette-divider">
-
-      <section class="row featurette bg-feature3"> <!-- feature3.jpg -->
-        <div class="col-md-7">
-          <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
-          <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div class="col-md-5">
-          <img class="featurette-image img-responsive" data-src="holder.js/500x500/auto" alt="Generic placeholder image">
-        </div>
-      </section> <!-- End of feature3 -->
-
-      <hr class="featurette-divider">
-    
+    <p class="pull-right"><a href="#home">Back to top <i class="fa fa-chevron-up"></i></a></p>    
     </section><!-- End of Container -->
     <!-- END OF FEATURETTES -->
