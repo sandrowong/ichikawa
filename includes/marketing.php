1983e9565300ef080aa123a5bdb0fbfaeda41b4f
Sandro Wong <swongkongfun@csupomona.edu>
2014-11-14 15:44:35 -0800

    changed button color and images

--- includes/marketing.php
+++ includes/marketing.php
@@ -10,21 +10,21 @@
           <img class="img-circle" src="img/marketing1.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
           <h2>Shabu</h2>
           <p> For Shabu Shabu, we have a choice of Beef (Certified Angus Beef), Kurobuta (Pork imported from Japan), and Chicken Breast.  Our Spicy Miso Soup is one of the crowd favorite for Shabu Shabu. A must try!</p>
-          <p><a class="btn btn-default" href="#" role="button">View menu &raquo;</a></p>
+          <p><a class="btn btn-default" href="<? echo $_SERVER["PHP_SELF"];?>?l=menu" role="button">View menu &raquo;</a></p>
         </section><!-- End of marketing1 -->
 
         <section class="col-lg-4 mark-box"> <!-- marketing2.jpg -->
           <img class="img-circle" src="img/marketing2.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
           <h2>Sushi</h2>
           <p>Rock Shrimp Roll is so far the best seller from our menu.  Rock Shrimp Roll is a Spicy Tuna Roll topped with eight big pieces of tempura rock shrimps, and the shrimp is tossed together with our creamy spicy mayo.</p>
-          <p><a class="btn btn-default" href="#" role="button">View menu &raquo;</a></p>
+          <p><a class="btn btn-default" href="<? echo $_SERVER["PHP_SELF"];?>?l=menu" role="button">View menu &raquo;</a></p>
         </section><!-- End of marketing2 -->
 
         <section class="col-lg-4 mark-box"> <!-- marketing3.jpg -->
           <img class="img-circle" src="img/marketing3.jpg" alt="Generic placeholder image" style="width: 140px; height: 140px;">
           <h2>Dessert</h2>
           <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
-          <p><a class="btn btn-default" href="#" role="button">View menu &raquo;</a></p>
+          <p><a class="btn btn-default" href="<? echo $_SERVER["PHP_SELF"];?>?l=menu" role="button">View menu &raquo;</a></p>
         </div><!-- End of marketing3 -->
 
       </div><!-- END OF MARKETING -->
--- includes/menu.php
+++ includes/menu.php
@@ -2,7 +2,8 @@
 
       <h1 class="main-underline">The Menu</h1>
       <p class="lead menu-info">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. <a href="ichikawa_menu.pdf" target="_blank">Download Menu</a></p>
-      <section class="row featurette bg-feature1"> <!-- feature1.jpg -->
+      
+      <section class="row featurette bg-feature1"> <!-- menu1 -->
         <div class="col-md-7">
           <h2 class="featurette-heading">First featurette heading.</h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
@@ -10,11 +11,11 @@
         <div class="col-md-5">
           <img class="featurette-image img-responsive" src="img/feature1.jpg" alt="Shabu">
         </div>        
-      </section> <!-- End of feature1 -->
+      </section> <!-- End of menu1 -->
 
       <hr class="featurette-divider">
-
-      <section class="row featurette bg-feature2"> <!-- feature2.jpg -->
+      
+      <section class="row featurette bg-feature2"> <!-- menu2 -->
         <div class="col-md-5">
           <img class="featurette-image img-responsive" src="img/menu/sushi1.jpg" alt="Sushi">
         </div>
@@ -22,11 +23,11 @@
           <h2 class="featurette-heading">Oh yeah</h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
         </div>
-      </section> <!-- End of feature2 -->
+      </section> <!-- End of menu2 -->
 
       <hr class="featurette-divider">
 
-      <section class="row featurette bg-feature3"> <!-- feature3.jpg -->
+      <section class="row featurette bg-feature3"> <!-- menu3 -->
         <div class="col-md-7">
           <h2 class="featurette-heading">First featurette heading.</h2>
           <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
@@ -34,6 +35,6 @@
         <div class="col-md-5">
           <img class="featurette-image img-responsive" src="img/menu/dessert1.jpg" alt="Dessert">
         </div>        
-      </section> <!-- End of feature1 -->
+      </section> <!-- End of menu3 -->
 
     </section><!-- End of Container -->
