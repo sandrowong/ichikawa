206560ef737adfe68c8ca2835474d49149eb643f
Sandro Wong <swongkongfun@csupomona.edu>
2014-11-14 14:50:19 -0800

    modal align center

--- includes/modal.php
+++ includes/modal.php
@@ -6,7 +6,7 @@
         <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
         <h4 class="modal-title" id="myModalLabel">Ichikawa - Fusion Shabu &amp; Sushi</h4>
       </div>
-      <div class="modal-body" style="text-aling: center">
+      <div class="modal-body" style="text-align: center">
         <video width="550" controls>
           <source src="video.mp4" type="video/mp4">
           Your browser does not support HTML5 video.
